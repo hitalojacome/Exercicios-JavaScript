a26af8f8ea57a4b8f34a80b0a9844fc784c249a3
hitalojacome <106604805+hitalojacome@users.noreply.github.com>
2023-05-29 21:23:48 -0300

    ex 4

--- Condicional/ex4/script.js
+++ Condicional/ex4/script.js
@@ -1,10 +1,10 @@
-const n1 = parseInt(prompt(nome + " digite o primeiro valor"))
-const n2 = parseInt(prompt("Digite o segundo valor"))
+let n1 = parseInt(prompt("Digite o primeiro valor"))
+let n2 = parseInt(prompt("Digite o segundo valor"))
 
 alert("Antes da troca: \nPrimeiro valor: " + n1 + "\nSegundo valor: " + n2);
 
-const temp = n1;
+let aux = n1;
 n1 = n2;
-n2 = temp;
+n2 = aux;
 
 alert("Depois da troca: \nPrimeiro valor: " + n1 + "\nSegundo valor: " + n2);
