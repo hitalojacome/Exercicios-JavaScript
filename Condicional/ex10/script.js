0cd1db7e4a82973bbf23c477677d1a01c8e53624
hitalojacome <106604805+hitalojacome@users.noreply.github.com>
2023-05-31 17:19:14 -0300

    ex10

---
+++ Condicional/ex10/script.js
@@ -0,0 +1,21 @@
+let nome = prompt('Olá! Qual seu nome?')
+let semana = prompt('Determinamos que domingo seja 1 e sábado seja 7, que dia da semana é hoje?')
+
+if (semana == 1) {
+    alert('Hoje é domingo')
+} else if (semana == 2) {
+    alert('Hoje é segunda feira')
+} else if (semana == 3) {
+    alert('Hoje é terça feira')
+} else if (semana == 4) {
+    alert('Hoje é quarta feira')
+} else if (semana == 5) {
+    alert('Hoje é quinta feira')
+} else if (semana == 6) {
+    alert('Hoje é sexta feira')
+} else if (semana == 7) {
+    alert('Hoje é sabado')
+} else {
+    alert('VALOR INVÁLIDO!')
+}
+
